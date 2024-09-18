daefd06d8b1b1111b662015619b750f468d737c8
Andrew Razin <razin.andrew.ru@mail.ru>
2024-09-19 00:15:11 +0300

    fixed rotating rotors

--- enigma_fixed.cpp
+++ enigma_fixed.cpp
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+void printBinary(char c)
+{
+    for (int i = 7; i >= 0; i--)
+    {
+        std::cout << ((c >> i) & 1);
+    }
+    std::cout << std::endl;
+}
+
 class Rotor
 {
 private:
@@ -12,6 +21,7 @@ private:
     int position;
     int alphabet_size;
     int offset;
+
 public:
     Rotor(std::vector<int> wiring, int position, int offset = 1) : wiring(wiring), position(position)
     {
@@ -30,7 +40,7 @@ public:
 
     char decrypt(char c)
     {
-        //cout<< "symbol" << c;
+        // cout<< "symbol" << c;
         for (char i = 0; i < alphabet_size; i++)
         {
             if (wiring[i] == c)
@@ -38,19 +48,19 @@ public:
                 return i;
             }
         }
-        //cout<< "symbol" << c;
+        // cout<< "symbol" << c;
         throw runtime_error("the symbol is not found in the alphabet:" + int(c));
         return -1;
     }
 
     void rotate()
     {
-        char temp_symbol = wiring[alphabet_size - 1];
-        for (int i = 0; i < alphabet_size; i++)
+        char temp_symbol = wiring[0];
+        for (int i = 0; i < alphabet_size - 1; i++)
         {
-            wiring[i + 1] = wiring[i];
+            wiring[i] = wiring[i + 1];
         }
-        wiring[0] = temp_symbol;
+        wiring[alphabet_size - 1] = temp_symbol;
     }
 };
 
@@ -62,7 +72,6 @@ private:
 public:
     Reflector(vector<pair<char, char>> &wiring) : connection(wiring)
     {
-       
     }
 
     char reflect(char c)
@@ -107,12 +116,7 @@ public:
         return c;
     }
 };
-void printBinary(char c) {
-    for (int i = 7; i >= 0; i--) {
-        std::cout << ((c >> i) & 1);
-    }
-    std::cout << std::endl;
-}
+
 class EnigmaMachine
 {
 public:
@@ -124,7 +128,7 @@ public:
 
     pair<char, char> split_byte(char byte)
     {
-      
+
         char high_half = (byte >> 4) & 0x0F; // shift right 4 bits and mask with 0x0F
         char low_half = byte & 0x0F;         // mask with 0x0F to get the low 4 bits
 
@@ -138,84 +142,91 @@ public:
         printBinary(reverse_splitting_bytes(low_half,high_half));
         cout<< endl;*/
 
-
-
         return make_pair(low_half, high_half);
     }
 
-    char reverse_splitting_bytes(char low_half,char high_half)
+    char reverse_splitting_bytes(char low_half, char high_half)
     {
         return (high_half << 4) | low_half;
     }
-   
+
     char encrypt_char(char c)
-    {   
-       // cout << "Char before encrypt:" << c << endl;
-       c = plugboard.swap(c);
-       // cout << "Char after plugboard swap:" << c <<endl;
+    {
+        // cout << "Char before encrypt:" << c << endl;
+        c = plugboard.swap(c);
+        // cout << "Char after plugboard swap:" << c <<endl;
         for (auto &rotor : rotors)
         {
             c = rotor.encrypt(c);
         }
-     //   cout << "Char after rotor swap:" << c <<endl;
+        //   cout << "Char after rotor swap:" << c <<endl;
         c = reflector.reflect(c);
-      //  cout << "Char after reflect swap:" << c <<endl;
+        //  cout << "Char after reflect swap:" << c <<endl;
         for (size_t i = rotors.size() - 1; i != -1; --i)
         {
             c = rotors[i].decrypt(c);
         }
 
-     //   cout << "Char after reverse rotor swap:" << c <<endl;
+        //   cout << "Char after reverse rotor swap:" << c <<endl;
         c = plugboard.swap(c);
-     //   cout << "Char after plugboard swap:" << c <<endl;
-
-
+        //   cout << "Char after plugboard swap:" << c <<endl;
 
         return c;
     }
 
-
     char decrypt_char(char c)
     {
-     
-      //  cout << "Char before decrypt:" << c << endl;
+
+        //  cout << "Char before decrypt:" << c << endl;
         c = plugboard.swap(c);
-     //   cout << "Char after plugboard:" << c << endl;
+        //   cout << "Char after plugboard:" << c << endl;
         for (auto &rotor : rotors)
         {
             c = rotor.encrypt(c);
         }
 
-      //  cout << "Char after rotor:" << c << endl;
+        //  cout << "Char after rotor:" << c << endl;
         c = reflector.reflect(c);
-    //     cout << "Char after reflect:" << c << endl;
-         for (size_t i = rotors.size() - 1; i != -1; --i)
+        //     cout << "Char after reflect:" << c << endl;
+        for (size_t i = rotors.size() - 1; i != -1; --i)
         {
             c = rotors[i].decrypt(c);
         }
-    //    cout << "Char after reverse rotor:" << c << endl;
+        //    cout << "Char after reverse rotor:" << c << endl;
         c = plugboard.swap(c);
-    //    cout << "Char after plugboard swap:" << c << endl;
+        //    cout << "Char after plugboard swap:" << c << endl;
 
         return c;
     }
 
-
+    void rotate_rotors()
+    {
+        int rotors_mod = 1;
+        for (int i = 0; i < rotors.size(); ++i)
+        {
+            if (turns_count % rotors_mod == 0)
+                {
+                    rotors[i].rotate();
+                }
+            rotors_mod *= rotors.size();
+        }
+    }
 
     string encrypt(string plaintext)
     {
         string encryptedText;
         for (char c : plaintext)
         {
-            pair<char,char> part_byte = split_byte(c);
+
+            pair<char, char> part_byte = split_byte(c);
             char lower_half = part_byte.first;
-            char high_half = part_byte.second; 
+            char high_half = part_byte.second;
             char encrypted_lower_half = encrypt_char(lower_half);
+            rotate_rotors();
             char encrypted_high_half = encrypt_char(high_half);
-            encryptedText += encrypted_lower_half;
-            //rotors[0].rotate();
+            rotate_rotors();
+            encryptedText += encrypted_lower_half;      
             encryptedText += encrypted_high_half;
-            //rotors[0].rotate();
         }
         return encryptedText;
     }
@@ -223,19 +234,19 @@ public:
     string decrypt(string ciphertext)
     {
         string decryptedText;
-        for (int i = 0; i < ciphertext.size(); i +=2)
+        for (int i = 0; i < ciphertext.size(); i += 2)
         {
             char lower_half = ciphertext[i];
-            char high_half = ciphertext[i+1];
+            char high_half = ciphertext[i + 1];
             char decrypted_lower_half = decrypt_char(lower_half);
+            rotate_rotors();
             char decrypted_high_half = decrypt_char(high_half);
-            char result = reverse_splitting_bytes(decrypted_lower_half,decrypted_high_half);
+            rotate_rotors();
+            char result = reverse_splitting_bytes(decrypted_lower_half, decrypted_high_half);
             decryptedText += result;
         }
         return decryptedText;
     }
-
-
 };
 
 int main()
@@ -244,38 +255,30 @@ int main()
     vector<Rotor> rotors1 = {
         Rotor({4, 10, 12, 5, 11, 6, 3, 15, 7, 14, 1, 13, 0, 2, 8, 9}, 2), // Rotor I
         Rotor({0, 9, 3, 10, 15, 8, 14, 13, 12, 11, 2, 7, 1, 6, 4, 5}, 1), // Rotor II
-       // Rotor({9, 12, 3, 4, 5, 11, 1, 2, 14, 7, 13, 15, 6, 10, 8, 0}, 12)  // Rotor III
+        Rotor({9, 12, 3, 4, 5, 11, 1, 2, 14, 7, 13, 15, 6, 10, 8, 0}, 12) // Rotor III
     };
 
     // Define reflector wiring (example)
     vector<pair<char, char>> reflector_wiring = {
-        {0, 4}, {1, 9}, {2, 12}, {3, 15}, {4, 0},
-        {5, 11}, {6, 14}, {7, 13}, {8, 10}, {9, 1},
-        {10, 8}, {11, 5}, {12, 2}, {13, 7}, {14, 6}, {15, 3}
-    };
+        {0, 4}, {1, 9}, {2, 12}, {3, 15}, {4, 0}, {5, 11}, {6, 14}, {7, 13}, {8, 10}, {9, 1}, {10, 8}, {11, 5}, {12, 2}, {13, 7}, {14, 6}, {15, 3}};
     Reflector reflector(reflector_wiring);
 
     // Define plugboard connections (example)
     vector<pair<char, char>> plugboard_connections = {
-        {0, 4}, {1, 9}, {2, 12}, {3, 15}, {4, 0}, 
-        {5, 11}, {6, 14}, {7, 13}, {8, 10}, {9, 1},
-        {10, 8}, {11, 5}, {12, 2}, {13, 7}, {14, 6}, {15, 3}
-    };
-   
+        {0, 4}, {1, 9}, {2, 12}, {3, 15}, {4, 0}, {5, 11}, {6, 14}, {7, 13}, {8, 10}, {9, 1}, {10, 8}, {11, 5}, {12, 2}, {13, 7}, {14, 6}, {15, 3}};
+
     Plugboard plugboard(plugboard_connections); // Correct constructor
 
     // Create an Enigma machine instance
-    EnigmaMachine enigma(rotors1, reflector, plugboard);
-
+    EnigmaMachine enigma1(rotors1, reflector, plugboard);
+    EnigmaMachine enigma2(rotors1,reflector,plugboard);
     // Example encryption and decryption
     string plaintext = "i need some sleep";
-    string ciphertext = enigma.encrypt(plaintext);
+    string ciphertext = enigma1.encrypt(plaintext);
     cout << "Text to encrypt:" << plaintext << endl;
     cout << "Ciphertext: " << ciphertext << endl;
-    string decryptedText = enigma.decrypt(ciphertext);
-    cout << "Decrypted Text: " << decryptedText << endl; 
-
-    
+    string decryptedText = enigma2.decrypt(ciphertext);
+    cout << "Decrypted Text: " << decryptedText << endl;
 
     return 0;
 }
