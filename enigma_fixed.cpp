f287236b7519b38aca1795c11222861be5ae4480
Andrew Razin <razin.andrew.ru@mail.ru>
2024-09-18 23:42:40 +0300

    fixed with num rotors

--- enigma_fixed.cpp
+++ enigma_fixed.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,7 +12,6 @@ private:
     int position;
     int alphabet_size;
     int offset;
-
 public:
     Rotor(std::vector<int> wiring, int position, int offset = 1) : wiring(wiring), position(position)
     {
@@ -39,7 +39,7 @@ public:
             }
         }
         //cout<< "symbol" << c;
-        throw runtime_error("the symbol is not found in the alphabet:" + string(1, c));
+        throw runtime_error("the symbol is not found in the alphabet:" + int(c));
         return -1;
     }
 
@@ -119,7 +119,7 @@ public:
     vector<Rotor> rotors;
     Reflector reflector;
     Plugboard plugboard;
-
+    int turns_count = 0;
     EnigmaMachine(vector<Rotor> rotors, Reflector reflector, Plugboard plugboard) : rotors(rotors), reflector(reflector), plugboard(plugboard) {}
 
     pair<char, char> split_byte(char byte)
@@ -150,24 +150,26 @@ public:
    
     char encrypt_char(char c)
     {   
-        cout << "Char before encrypt:" << c << endl;
-        c = plugboard.swap(c);
-        cout << "Char after plugboard swap:" << c <<endl;
+       // cout << "Char before encrypt:" << c << endl;
+       c = plugboard.swap(c);
+       // cout << "Char after plugboard swap:" << c <<endl;
         for (auto &rotor : rotors)
         {
             c = rotor.encrypt(c);
         }
-        cout << "Char after rotor swap:" << c <<endl;
+     //   cout << "Char after rotor swap:" << c <<endl;
         c = reflector.reflect(c);
-        cout << "Char after reflect swap:" << c <<endl;
-        for (auto &rotor : rotors)
+      //  cout << "Char after reflect swap:" << c <<endl;
+        for (size_t i = rotors.size() - 1; i != -1; --i)
         {
-            c = rotor.decrypt(c);
+            c = rotors[i].decrypt(c);
         }
 
-        cout << "Char after reverse rotor swap:" << c <<endl;
+     //   cout << "Char after reverse rotor swap:" << c <<endl;
         c = plugboard.swap(c);
-        cout << "Char after plugboard swap:" << c <<endl;
+     //   cout << "Char after plugboard swap:" << c <<endl;
+
+
 
         return c;
     }
@@ -175,24 +177,25 @@ public:
 
     char decrypt_char(char c)
     {
-        cout << "Char before decrypt:" << c << endl;
+     
+      //  cout << "Char before decrypt:" << c << endl;
         c = plugboard.swap(c);
-        cout << "Char after plugboard:" << c << endl;
+     //   cout << "Char after plugboard:" << c << endl;
         for (auto &rotor : rotors)
         {
             c = rotor.encrypt(c);
         }
 
-        cout << "Char after rotor:" << c << endl;
+      //  cout << "Char after rotor:" << c << endl;
         c = reflector.reflect(c);
-         cout << "Char after reflect:" << c << endl;
-        for (auto &rotor : rotors)
+    //     cout << "Char after reflect:" << c << endl;
+         for (size_t i = rotors.size() - 1; i != -1; --i)
         {
-            c = rotor.decrypt(c);
+            c = rotors[i].decrypt(c);
         }
-        cout << "Char after reverse rotor:" << c << endl;
+    //    cout << "Char after reverse rotor:" << c << endl;
         c = plugboard.swap(c);
-        cout << "Char after plugboard swap:" << c << endl;
+    //    cout << "Char after plugboard swap:" << c << endl;
 
         return c;
     }
@@ -231,31 +234,24 @@ public:
         }
         return decryptedText;
     }
+
+
 };
 
 int main()
 {
     // Example usage: Define different rotor configurations
     vector<Rotor> rotors1 = {
-        Rotor({4, 10, 12, 5, 11, 6, 3, 15, 7, 14, 1, 13, 0, 2, 8, 9, 20, 26, 28, 21, 27, 22, 19, 25, 23, 24, 16, 17, 18, 30, 36, 38, 31, 37, 32, 35, 39, 33, 34, 40, 46, 48, 41, 47, 42, 45, 49, 43, 44, 50, 56, 58, 51, 57, 52, 55, 59, 53, 54, 60, 62, 63, 61}, 12), // Rotor I
-        //Rotor({0, 9, 3, 10, 15, 8, 14, 13, 12, 11, 2, 7, 1, 6, 4, 5}, 1), // Rotor II
-        //Rotor({1, 15, 13, 12, 10, 11, 14, 2, 14, 5, 9, 8, 0, 7, 3, 4}, 2)  // Rotor III
+        Rotor({4, 10, 12, 5, 11, 6, 3, 15, 7, 14, 1, 13, 0, 2, 8, 9}, 2), // Rotor I
+        Rotor({0, 9, 3, 10, 15, 8, 14, 13, 12, 11, 2, 7, 1, 6, 4, 5}, 1), // Rotor II
+       // Rotor({9, 12, 3, 4, 5, 11, 1, 2, 14, 7, 13, 15, 6, 10, 8, 0}, 12)  // Rotor III
     };
+
     // Define reflector wiring (example)
-     vector<pair<char, char>> reflector_wiring = {
+    vector<pair<char, char>> reflector_wiring = {
         {0, 4}, {1, 9}, {2, 12}, {3, 15}, {4, 0},
         {5, 11}, {6, 14}, {7, 13}, {8, 10}, {9, 1},
-        {10, 8}, {11, 5}, {12, 2}, {13, 7}, {14, 6}, {15, 3},
-        {16, 20}, {17, 25}, {18, 28}, {19, 23}, {20, 16}, 
-        {21, 27}, {22, 30}, {23, 29}, {24, 26}, {25, 17},
-        {26, 24}, {27, 21}, {28, 18}, {29, 23}, {30, 22}, 
-        {31, 35}, {32, 39}, {33, 42}, {34, 45}, {35, 31},
-        {36, 41}, {37, 44}, {38, 43}, {39, 32}, {40, 40}, 
-        {41, 36}, {42, 33}, {43, 38}, {44, 37}, {45, 34},
-        {46, 50}, {47, 55}, {48, 58}, {49, 53}, {50, 46}, 
-        {51, 57}, {52, 60}, {53, 59}, {54, 56}, {55, 47},
-        {56, 54}, {57, 51}, {58, 48}, {59, 53}, {60, 52}, 
-        {61, 63}, {62, 61}, {63, 62}
+        {10, 8}, {11, 5}, {12, 2}, {13, 7}, {14, 6}, {15, 3}
     };
     Reflector reflector(reflector_wiring);
 
@@ -263,18 +259,9 @@ int main()
     vector<pair<char, char>> plugboard_connections = {
         {0, 4}, {1, 9}, {2, 12}, {3, 15}, {4, 0}, 
         {5, 11}, {6, 14}, {7, 13}, {8, 10}, {9, 1},
-        {10, 8}, {11, 5}, {12, 2}, {13, 7}, {14, 6}, {15, 3},
-        {16, 20}, {17, 25}, {18, 28}, {19, 23}, {20, 16}, 
-        {21, 27}, {22, 30}, {23, 29}, {24, 26}, {25, 17},
-        {26, 24}, {27, 21}, {28, 18}, {29, 23}, {30, 22}, 
-        {31, 35}, {32, 39}, {33, 42}, {34, 45}, {35, 31},
-        {36, 41}, {37, 44}, {38, 43}, {39, 32}, {40, 40}, 
-        {41, 36}, {42, 33}, {43, 38}, {44, 37}, {45, 34},
-        {46, 50}, {47, 55}, {48, 58}, {49, 53}, {50, 46}, 
-        {51, 57}, {52, 60}, {53, 59}, {54, 56}, {55, 47},
-        {56, 54}, {57, 51}, {58, 48}, {59, 53}, {60, 52}, 
-        {61, 63}, {62, 61}, {63, 62}
+        {10, 8}, {11, 5}, {12, 2}, {13, 7}, {14, 6}, {15, 3}
     };
+   
     Plugboard plugboard(plugboard_connections); // Correct constructor
 
     // Create an Enigma machine instance
